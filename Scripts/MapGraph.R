8450c232068d27b8782ee48c2f6846b30d3a0f05
Dominick Tavitian <dominicktavitian@gmail.com>
2017-05-30 14:52:31 -0700

    Fix map

--- Scripts/MapGraph.R
+++ Scripts/MapGraph.R
@@ -5,23 +5,6 @@ library(plyr)
 library(tidyr)
 #Set Working Directory:
 #setwd("~/Documents/Spring2017/INFO201/final/info-201-final-project/Scripts")
-data.set.month <- read.csv("./Data/Marijuana_Use_Past_Month.csv") 
-data.set.year <-  read.csv("./Data/Marijuana_Use_Past_Year.csv")
-
-is.data.frame(data.set.month)
-is.data.frame(data.set.year)
-
-
-#create new column with no percent sign so we can calculate values
-updated.month.data <- mutate(data.set.month, percents = extract_numeric(Small..Area.Estimate))
-View(updated.month.data)
-updated.year.data <- mutate(data.set.year, percents = extract_numeric(Small..Area.Estimate))
-
-#Organize percentages smoked by State
-avg.month.data <- ddply(updated.month.data, .(State), summarize, percents = mean(percents), code)
-View(avg.month.data)
-avg.year.data <- ddply(updated.year.data, .(State), summarize, percents = mean(percents))
-
 
 #function for building a map
 BuildMap <- function(avg.data){
@@ -36,7 +19,7 @@ BuildMap <- function(avg.data){
   
   AvgMonthYear <- plot_geo(avg.data, locationmode = 'USA-states') %>%
     add_trace(
-      z = ~percents, text = ~percents, locations = ~Abbreviation,
+      z = ~percents, locations = ~Abbreviation,
       color = ~percents, colors = 'Greens'
     ) %>%
     colorbar(title = "Week Smoked") %>%
--- server.R
+++ server.R
@@ -12,10 +12,10 @@ source('./Scripts/Bar Graph.R')
 data.set.month <- read.csv("./data/Marijuana_Use_Past_Month.csv") 
 data.set.year <-  read.csv("./data/Marijuana_Use_Past_Year.csv")
 states.data <- read.csv("./data/States.csv")
-is.data.frame(data.set.month)
-is.data.frame(data.set.year)
-is.data.frame(states.data)
-View(states.data)
+#is.data.frame(data.set.month)
+#is.data.frame(data.set.year)
+#is.data.frame(states.data)
+#View(states.data)
 # Create new column with no percent sign so we can calculate values
 updated.month.data <- mutate(data.set.month, percents = extract_numeric(Small..Area.Estimate))
 
@@ -23,12 +23,13 @@ updated.year.data <- mutate(data.set.year, percents = extract_numeric(Small..Are
 
 # Organize percentages smoked by State
 avg.month.data <- ddply(updated.month.data, .(State), summarize, percents = mean(percents))
-View(avg.month.data)
+#View(avg.month.data)
 new.avg.month.data <- merge(avg.month.data, states.data, by = "State")
-View(new.avg.month.data)
+#View(new.avg.month.data)
 avg.year.data <- ddply(updated.year.data, .(State), summarize, percents = mean(percents))
 new.avg.year.data <- merge(avg.year.data, states.data, by = "State")
-View(new.avg.year.data)
+#View(new.avg.year.data)
+#############
 
 shinyServer(function(input, output) { 
 
