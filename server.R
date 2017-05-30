df6907971f75a334279938ca3b30fde83bf25dbb
Sean Meharry <seanmeharry@D-173-250-152-18.dhcp4.washington.edu>
2017-05-30 12:37:37 -0700

    Fixed Bar

--- server.R
+++ server.R
@@ -1,18 +1,50 @@
 # server.r 
 library(shiny)
 
-setwd("C:/Users/Julia/Desktop/Info201/info-201-final-project")
+#setwd("C:/Users/Julia/Desktop/Info201/info-201-final-project")
 
-marijuana.laws <- read.csv('./Data/legal_marijuana_support.csv', stringsAsFactors = FALSE)
-source('./scripts/Timeline.R')
+marijuana.laws <- read.csv('./data/legal_marijuana_support.csv', stringsAsFactors = FALSE)
+source('./Scripts/Timeline.R')
+source('./Scripts/MapGraph.r')
+
+##Domincks data manipulation. 
+data.set.month <- read.csv("./data/Marijuana_Use_Past_Month.csv") 
+data.set.year <-  read.csv("./data/Marijuana_Use_Past_Year.csv")
+
+is.data.frame(data.set.month)
+is.data.frame(data.set.year)
+
+View(data.set.month)
+View(data.set.year)
+
+#create new column with no percent sign so we can calculate values
+updated.month.data <- mutate(data.set.month, percents = extract_numeric(Small..Area.Estimate))
+
+updated.year.data <- mutate(data.set.year, percents = extract_numeric(Small..Area.Estimate))
+
+#Organize percentages smoked by State
+avg.month.data <- ddply(updated.month.data, .(State), summarize, percents = mean(percents))
+View(avg.month.data)
+
+avg.year.data <- ddply(updated.year.data, .(State), summarize, percents = mean(percents))
+View(avg.year.data)
+
 
 shinyServer(function(input, output) { 
   
   
   
   
-  
-  
+  ##Map  with Widget
+  output$AvgMonthYear <- renderPlotly({
+    if(input$var == "Month"){
+      avg.data <- avg.month.data
+    } else{
+      avg.data <- avg.year.data
+    }
+    
+    return(BuildMap(avg.data))
+  })
   
   ##Timeline with Widgit 
   output$timeline <- renderPlotly({ 
--- ui.R
+++ ui.R
@@ -4,24 +4,67 @@
 library(shiny)
 library(plotly)
 
-shinyUI(fluidPage(
+shinyUI(navbarPage('Marijuana Timeline',
+                  tabPanel("Timeline for Marijuana Legalization",
+                  
+                          
+                          # Create sidebar layout
+                          sidebarLayout(
+                            
+                          # Side panel for controls
+                          sidebarPanel(
+                            
+                          #Widget   
+                            selectInput('yvar', label = 'Variable to Graph', choices = list("% For Legalization" = 'Percent_Yes', '% Against Legalization' = 'No_Illegal', '% No Opinion' = 'No_Opinion'))
+                          ),      
+
+                      
+                          # Main panel: display data
+                            mainPanel(
+                              #output datavis
+                                plotlyOutput('timeline')
+                            )
+                          )
+                 ),
+                 tabPanel('Map',
+                          # Create a sidebar layout for this tab (page)
+                          sidebarLayout(
+                            sidebarPanel(
+                              p('States with Most Usage'),
+                              
   
-  # Create sidebar layout
-  sidebarLayout(
-    
-    # Side panel for controls
-    sidebarPanel(
-      
-    #Widget   
-      selectInput('yvar', label = 'Variable to Graph', choices = list("% For Legalization" = 'Percent_Yes', '% Against Legalization' = 'No_Illegal', '% No Opinion' = 'No_Opinion'))
-    ),      
-
-
-    # Main panel: display data
-      mainPanel(
-        #output datavis
-          plotlyOutput('timeline')
-      )
+                              selectInput(inputId = "var",
+                                          label = "Month vs. Year:",
+                                          choices = c("Month", "Year"),
+                                          selected = "Month")
+                              
+                            ),
+                            
+                            # Show a plot of the generated distribution
+                            mainPanel(
+                              plotlyOutput("AvgMonthYear")
+                            )
+                          )
+                          
+                ),
+                tabPanel('Bar Graph',
+                         
+                         # Create a sidebar layout for this tab (page)
+                         sidebarLayout(
+                           
+                           # Create a sidebarPanel for your controls
+                         sidebarPanel(
+                             
+                             #Widet
+                             
+                             
+                         ),
+                         mainPanel(
+                           #output datavis
+                           plotlyOutput('timeline')
+                         )
+                         )
+                         
+                )
     )
   )
-)
