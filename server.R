6f2dd5884cd31bcac92a80d15c597db18ec61f76
Dominick Tavitian <dominicktavitian@gmail.com>
2017-05-30 11:44:05 -0700

    Update map function

--- server.R
+++ server.R
@@ -6,13 +6,38 @@ setwd("C:/Users/Julia/Desktop/Info201/info-201-final-project")
 marijuana.laws <- read.csv('./Data/legal_marijuana_support.csv', stringsAsFactors = FALSE)
 source('./scripts/Timeline.R')
 
+##Domincks data manipulatoin. 
+data.set.month <- read.csv("./data/Marijuana_Use_Past_Month.csv") 
+data.set.year <-  read.csv("./data/Marijuana_Use_Past_Year.csv")
+
+is.data.frame(data.set.month)
+is.data.frame(data.set.year)
+
+View(data.set.month)
+View(data.set.year)
+
+#create new column with no percent sign so we can calculate values
+updated.month.data <- mutate(data.set.month, percents = extract_numeric(Small..Area.Estimate))
+
+updated.year.data <- mutate(data.set.year, percents = extract_numeric(Small..Area.Estimate))
+
+#Organize percentages smoked by State
+avg.month.data <- ddply(updated.month.data, .(State), summarize, percents = mean(percents))
+View(avg.month.data)
+
+avg.year.data <- ddply(updated.year.data, .(State), summarize, percents = mean(percents))
+View(avg.year.data)
+
+
 shinyServer(function(input, output) { 
   
   
   
   
   
-  
+  output$AvgMonth <- renderPlotly({
+    return(BuildMap(avg.data))
+  })
   
   ##Timeline with Widgit 
   output$timeline <- renderPlotly({ 
