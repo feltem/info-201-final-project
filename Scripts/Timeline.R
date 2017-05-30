e6fe9da2b13dbc2c603f07698aa9872ac79f7564
Julia Weaver <juliaw33@uw.edu>
2017-05-30 11:43:36 -0700

    fixed scripts

---
+++ Scripts/Timeline.R
@@ -0,0 +1,25 @@
+#Timeline of Marijuana laws by state
+
+library(dplyr)
+library(plotly)
+library(ggplot2)
+
+#setwd("C:/Users/Julia/Desktop/Info201/info-201-final-project/scripts")
+
+#marijuana.laws <- read.csv('../Data/legal_marijuana_support.csv', stringsAsFactors = FALSE)
+
+BuildTimeline <- function(data, yvar){
+  
+  var.equation <- paste0('~', yvar)
+  
+  timeline <- plot_ly(data=data, x = ~Year, 
+          y = eval(parse(text = var.equation)),
+          type = 'scatter', mode = 'lines+markers')%>%
+    layout(title = "National Views on Marijuana Legalization (1969-2016)")
+  
+  
+  
+  return(timeline)
+}
+
+
